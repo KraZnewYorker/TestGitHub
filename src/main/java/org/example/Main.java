cea9630d5d54532e3b051f574b67a23dceeaf60d
KraZnewYorker <1695260@bdeb.qc.ca>
2022-12-06 15:41:45 -0500

    s

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello Zakia");
+
+//        System.out.println("voici mon code partager");
+        System.out.println("Bonjour c est zakia");
+    }
+}
